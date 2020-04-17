3082b0331243f6ebeeb1ffa83fdfddd717111fb6
vetleen <vetleengesbak@gmail.com>
2020-04-17 07:55:24 +0200

    Update settings.py

--- vetletemplate/settings.py
+++ vetletemplate/settings.py
@@ -148,16 +148,24 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ## READY FOR PRODUCTION? ##
-#CSRF_COOKIE_SECURE = True
-#SESSION_COOKIE_SECURE = True
-#X_FRAME_OPTIONS = 'DENY'
-##To enable testing in local env run: export DJANGO_SECURE_SSL_REDIRECT=False
-#SECURE_SSL_REDIRECT = os.environ.get('DJANGO_SECURE_SSL_REDIRECT', True)
-#SECURE_BROWSER_XSS_FILTER = True
-#SECURE_CONTENT_TYPE_NOSNIFF= True
-#SECURE_HSTS_SECONDS=3600 # When certain this does not break anything set to 31536000 which is one year // If it DOES break somehting, set it to 0, wait an hour+, and it should be fixed
-#SECURE_HSTS_INCLUDE_SUBDOMAINS=True
-#SECURE_HSTS_PRELOAD=True
+#CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', True)
+#SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', True)
+#X_FRAME_OPTIONS = os.environ.get('X_FRAME_OPTIONS', 'DENY') #'DENY'
+
+#SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', True)
+#SECURE_BROWSER_XSS_FILTER = os.environ.get('SECURE_BROWSER_XSS_FILTER', True)
+#SECURE_CONTENT_TYPE_NOSNIFF= os.environ.get('SECURE_CONTENT_TYPE_NOSNIFF', True)
+#SECURE_HSTS_SECONDS= os.environ.get('SECURE_HSTS_SECONDS', 0)#3600 # When certain this does not break anything set to 31536000 which is one year // If it DOES break somehting, set it to 0, wait an hour+, and it should be fixed
+#SECURE_HSTS_INCLUDE_SUBDOMAINS=os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', False)
+#SECURE_HSTS_PRELOAD=os.environ.get('SECURE_HSTS_PRELOAD', False)
+
+##Made a mistake? Chrome:
+#In the address bar, type “chrome://net-internals/#hsts”.
+#Type the domain name in the text field below “Delete domain”.
+#Click the “Delete” button.
+#Type the domain name in the text field below “Query domain”.
+#Click the “Query” button.
+#Your response should be “Not found”.
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
