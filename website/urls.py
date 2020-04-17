0e1eaf18eb4ae7ce1ea4d6c7105ceefb74c3bb6a
vetleen <vetleengesbak@gmail.com>
2020-04-17 07:57:08 +0200

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,28 @@
 from django.urls import path
 from . import views
 
+print ('OS says: %s'%(os.environ.get('CSRF_COOKIE_SECURE', "fuzzballs")))
+print ('Django says: %s'%(settings.CSRF_COOKIE_SECURE))
+print ('')
+print ('OS says: %s'%(os.environ.get('SESSION_COOKIE_SECURE', "fuzzballs")))
+print ('Django says: %s'%(settings.SESSION_COOKIE_SECURE))
+print ('')
+print ('OS says: %s'%(os.environ.get('X_FRAME_OPTIONS', "fuzzballs")))
+print ('Django says: %s'%(settings.X_FRAME_OPTIONS))
+print ('')
+print ('OS says: %s'%(os.environ.get('SECURE_SSL_REDIRECT', "fuzzballs")))
+print ('Django says: %s'%(settings.SECURE_SSL_REDIRECT))
+print ('')
+print ('OS says: %s'%(os.environ.get('SECURE_BROWSER_XSS_FILTER', "fuzzballs")))
+print ('Django says: %s'%(settings.SECURE_BROWSER_XSS_FILTER))
+print ('')
+print ('OS says: %s'%(os.environ.get('SECURE_CONTENT_TYPE_NOSNIFF', "fuzzballs")))
+print ('Django says: %s'%(settings.SECURE_CONTENT_TYPE_NOSNIFF))
+print ('')
+#print ('OS says: %s'%(os.environ.get('SECURE_HSTS_SECONDS', "fuzzballs")))
+#print ('Django says: %s'%(settings.SECURE_HSTS_SECONDS))
+#print ('')
+
 urlpatterns = [
 
     path('', views.index, name='index'),
